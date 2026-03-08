f4bc502b7aae58f31ed8671e3e6c431ebead7149
socialhubsmondial-droid <socialhubsmondial@gmail.com>
2026-03-08 05:59:46 +0100

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
